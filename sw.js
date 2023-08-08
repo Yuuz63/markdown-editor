c9224751108cbe5122b9a522afa4db72eaff15f3
yitoigawa <yitoigawa@fab.pasona.tech>
2023-08-08 13:35:27 +0900

    app完成 webアプリマニフェストの使用

--- sw.js
+++ sw.js
@@ -9,18 +9,17 @@ self.addEventListener('activate', (event) => {
 })
 
 const networkFallingBackToCache = async (request) => {
-    const cache = await caches.open(CacheName)
-    try {
-      const response = await fetch(request)
-      await cache.put(request, response.clone())
-      return response
-    } catch (err) {
-      console.error(err)
-      return cache.match(request)
-    }
+  const cache = await caches.open(CacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (err) {
+    console.error(err)
+    return cache.match(request)
   }
+}
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(networkFallingBackToCache(event.request))
-
+  event.respondWith(networkFallingBackToCache(event.request))
 })
